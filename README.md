7751f3e4e320100acc8a7f367b397ed3117d869c
linjianguang <1419479392@qq.com>
2024-09-23 17:22:12 +0800

    docs: add description

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # vite-plugin-html-public-path
+
+why need this plugin? sometime, we need to set the `public path` dynamically, for example, when we use `vite` to build the project, we need to set the `public path` according to the environment. but, we can't set the `public path` in the `index.html` file, so we need to use this plugin to transform all static `link/script` tags to dynamic tags
+
 implement dynamic public path in index.html
+
+![alt text](image.png)
 
 ## Install
 
--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import htmlPublicPath from 'vite-plugin-html-public-path'
+// import htmlPublicPath from 'vite-plugin-html-public-path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    htmlPublicPath({
-      // it will be injected to head of script tag
-      initialPrefixScript: `
-        const prefix = window.__PREFIX__ = "/customPrefix";
-        return prefix;
-      `
-    })
+    // htmlPublicPath({
+    //   // it will be injected to head of script tag
+    //   initialPrefixScript: `
+    //     const prefix = window.__PREFIX__ = "/customPrefix";
+    //     return prefix;
+    //   `
+    // })
   ],
 })
